2fb396f5dc1dcc04a3f5fa01797782994f5ef8eb
Fernandosoares1982 <fernando.7995@aluno.pr.senac.br>
2026-08-19 14:10:00 -0300

    Add files via upload

---
+++ Aula18082026/src/main/java/com/aula18082026/model/Primeira.java
@@ -0,0 +1,11 @@
+package main.java.com.aula18082026.model;
+
+import java.util.Scanner;
+
+public class Primeira {
+
+	public static void main(String[] args) {
+		//System.out.println("Olá Mundo");
+		Scanner leitor=new Scanner();
+	}
+}
